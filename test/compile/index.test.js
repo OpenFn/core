5fff8e913d8291b4984523ff67bc4cc1ba395bfc
Stuart Corbishley <corbish@gmail.com>
2016-09-06 16:04:31 +0200

    Introduced Compiler and transforms.
    Another big commit.
    
    * Compiler class for working with code and errors.
    * Transforms:
      verify - checks that all call expressions are valid,
        by injecting the sandbox in the code and looking up scope.
      wrapRootExpressions - takes all root call expressions and wraps them
        i.e. execute(foo(), bar());
      callFunction & wrapIIFE - previous boilerplate to get the whole
        expression to be called with state. Now in the compiler to maintain
        sourcemaps (for later). Also simplifies the Execution env.
    
    * Utils getModule now gets the memberName if specified.
      i.e. 'language-foo.Adaptor' => require('language-foo').Adaptor;
    * Tests! Hooray!
    
    * CLI module now can throw errors from the compiler

---
+++ test/compile/index.test.js
@@ -0,0 +1,72 @@
+const assert = require('assert');
+const Compile = require('../../lib/compile');
+const {
+  verify, wrapRootExpressions, callFunction, wrapIIFE
+} = require('../../lib/compile/transforms');
+
+describe("Compile", () => {
+  it("does something", () => {
+
+    let code = `each([1,2,3], (i) => { return i++ })`
+
+    new Compile(code, []);
+  })
+
+  it("throws an error when a transform doesn't return the ast", () => {
+    assert.throws(
+      () => { new Compile("", [function() {}]) },
+      /A transform unexpectedly returned undefined/
+    )
+  })
+})
+
+describe("Transforms", () => {
+
+  function assertCodeEqual(compile, expected) {
+    return assert.equal(compile.toString(), expected);
+  }
+
+  describe("verify", () => {
+    it("checks the scope of all call expressions", () => {
+      let result
+
+      result = new Compile(`noExist()`, [verify({ sandbox: { } })]);
+      assert(result.errors.length > 0)
+
+      result = new Compile(`exist()`, [verify({ sandbox: { exist: null } })]);
+      assert(result.errors.length == 0)
+    })
+
+    it("returns the code unmodified", () => {
+      result = new Compile(`foo()`, [verify({ sandbox: { foo: null } })])
+      assertCodeEqual(result, 'foo()')
+    })
+
+    it("specifies the line and col numbers for errors.")
+  })
+
+  describe("wrapRootExpresssions", () => {
+    it("wraps all root expressions in a function", () => {
+      result = new Compile(`foo();bar();baz();`, [wrapRootExpressions('execute')])
+      assertCodeEqual(result, 'execute(foo(), bar(), baz());')
+    })
+  })
+
+  describe("callFunction", () => {
+    it("calls callExpression with indentifer", () => {
+      result = new Compile(`execute(foo(), bar(), baz())`, [callFunction('execute', 'state')])
+      assertCodeEqual(result, 'execute(foo(), bar(), baz())(state)')
+    })
+  })
+
+  describe("wrapIIFE", () => {
+    it("wraps the body in an IIFE", () => {
+      result = new Compile(`execute(foo(), bar(), baz())`, [wrapIIFE()])
+      assertCodeEqual(result, [
+        "(function() {",
+        "  return execute(foo(), bar(), baz());",
+        "})()"
+      ].join("\n"))
+    })
+  })
+})
