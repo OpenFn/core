8fdf6e50a609524561b7d514f4b0bb989f601f21
Taylor Downs <downs.taylor@gmail.com>
2020-05-28 10:15:58 -0600

    include initial errors array in Recast's Acorn AST

--- test/compile/index.test.js
+++ test/compile/index.test.js
@@ -1,71 +1,79 @@
 const assert = require('assert');
 const Compile = require('../../lib/compile');
 const {
-  verify, wrapRootExpressions, callFunction, wrapIIFE
+  verify,
+  wrapRootExpressions,
+  callFunction,
+  wrapIIFE,
 } = require('../../lib/compile/transforms');
 
-describe("Compile", () => {
+describe('Compile', () => {
   it("throws an error when a transform doesn't return the ast", () => {
-    assert.throws(
-      () => { new Compile("", [function() {}]) },
-      /A transform unexpectedly returned undefined/
-    )
-  })
-})
+    assert.throws(() => {
+      new Compile('', [function () {}]);
+    }, /A transform unexpectedly returned undefined/);
+  });
+});
 
-describe("Transforms", () => {
-
+describe('Transforms', () => {
   function assertCodeEqual(compile, expected) {
     return assert.equal(compile.toString(), expected);
   }
 
-  describe("verify", () => {
-    it("checks the scope of all call expressions", () => {
-      let result
+  describe('verify', () => {
+    it('checks the scope of all call expressions', () => {
+      let result;
 
-      result = new Compile(`noExist()`, [verify({ sandbox: { } })]);
-      assert(result.errors.length > 0)
+      result = new Compile(`noExist()`, [verify({ sandbox: {} })]);
+      assert(result.errors.length > 0);
 
       result = new Compile(`exist()`, [verify({ sandbox: { exist: null } })]);
-      assert(result.errors.length == 0)
-    })
+      assert(result.errors.length == 0);
+    });
 
-    it("returns the code unmodified", () => {
-      result = new Compile(`foo()`, [verify({ sandbox: { foo: null } })])
-      assertCodeEqual(result, 'foo()')
-    })
+    it('returns the code unmodified', () => {
+      result = new Compile(`foo()`, [verify({ sandbox: { foo: null } })]);
+      assertCodeEqual(result, 'foo()');
+    });
 
-    it("specifies the line and col numbers for errors.")
-  })
+    it('specifies the line and col numbers for errors.');
+  });
 
-  describe("wrapRootExpresssions", () => {
-    it("wraps all root expressions in a function", () => {
-      result = new Compile(`foo();bar();baz();`, [wrapRootExpressions('execute')])
-      assertCodeEqual(result, 'execute(foo(), bar(), baz());')
-    })
+  describe('wrapRootExpresssions', () => {
+    it('wraps all root expressions in a function', () => {
+      result = new Compile(`foo();bar();baz();`, [
+        wrapRootExpressions('execute'),
+      ]);
+      assertCodeEqual(result, 'execute(foo(), bar(), baz());');
+    });
 
     it("won't break if the last character is a space", () => {
-      const code = `each() `
-      result = new Compile(code, [wrapRootExpressions('execute')])
-      assertCodeEqual(result, 'execute(each());')
-    })
-  })
+      const code = `each() `;
+      result = new Compile(code, [wrapRootExpressions('execute')]);
+      assertCodeEqual(result, 'execute(each());');
+    });
+  });
 
-  describe("callFunction", () => {
-    it("calls callExpression with indentifer", () => {
-      result = new Compile(`execute(foo(), bar(), baz())`, [callFunction('execute', 'state')])
-      assertCodeEqual(result, 'execute(foo(), bar(), baz())(state)')
-    })
-  })
+  describe('callFunction', () => {
+    it('calls callExpression with indentifer', () => {
+      result = new Compile(`execute(foo(), bar(), baz())`, [
+        callFunction('execute', 'state'),
+      ]);
+      assertCodeEqual(result, 'execute(foo(), bar(), baz())(state)');
+    });
+  });
 
-  describe("wrapIIFE", () => {
-    it("wraps the body in an IIFE", () => {
-      result = new Compile(`execute(foo(), bar(), baz())`, [wrapIIFE()])
-      assertCodeEqual(result, [
-        "(function() {",
-        "  return execute(foo(), bar(), baz());",
-        "})();"
-      ].join("\n"))
-    })
-  })
-})
+  describe('wrapIIFE', () => {
+    it('wraps the body in an IIFE', () => {
+      result = new Compile(`execute(foo(), bar(), baz())`, [wrapIIFE()]);
+      assertCodeEqual(
+        result,
+        [
+          '(function() {',
+          '  return execute(foo(), bar(), baz());',
+          '})();',
+        ].join('\n')
+      );
+    });
+  });
+});
