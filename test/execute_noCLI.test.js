5bec3999457e505a37c363ec19bf77e99429e266
Marco Guimaräes <marcopereira@gmx.ch>
2020-10-29 08:40:11 +0100

    separated execution from preparation

--- test/execute_noCLI.test.js
+++ test/execute_noCLI.test.js
@@ -1,33 +1,27 @@
 const assert = require('assert');
-const Execute_noCLI = require('../lib/execute_noCLI');
+const { Execute, Execute_noCLI } = require('../lib/execute_noCLI');
 
-describe("Execute_noCLI", () => {
-    it("expects an expression", () => {
-        assert.throws(
-            () => { Execute_noCLI() },
-            /Cannot execute without an expression./
-        )
-    })
+describe('Execute_noCLI', () => {
+    it('expects an expression', () => {
+        assert.throws(() => {
+            Execute();
+        }, /Cannot execute without an expression./);
+    });
 
-    it("expects an initial state", () => {
-        assert.throws(
-            () => { Execute_noCLI("foo") },
-            /Cannot execute without an initial state./
-        )
-    })
+    it('expects an initial state', () => {
+        assert.throws(() => {
+            Execute({ code: 'foo' });
+        }, /Cannot execute without an initial state./);
+    });
 
-    describe("when given an expression and state", () => {
-        it("returns", async () => {
-            let result = await Execute_noCLI(
-                `add(1)`,
-                {"num":1},
-                { add: num => state => {
-                        return state.num + num } }
-            )
-            assert.equal(result, 2)
-        })
-    })
-
-
-
-})
+    describe('when given an expression and state', () => {
+        it('returns', async () => {
+            let result = await Execute(`add(1)(state)`, 1, {
+                add: num => state => {
+                    return state + num;
+                }
+            });
+            assert.equal(result, 2);
+        });
+    });
+});
