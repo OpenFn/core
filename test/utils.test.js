3a0102811fdbcc73376f1a1897e96120c1e8fef8
Stuart Corbishley <corbish@gmail.com>
2021-02-04 14:35:40 +0200

    Allow npm style requires
    **BREAKING CHANGES**
    
    - `language-pack.Adaptor` style will no longer work
      use `language-pack/Adaptor` or `language-pack` if the default export
      of the module is `Adaptor`.
    
    _still to do_
    
    - Tests for the new `getModuleDetails` function
    - Abstract the double `safeResolve` in the execute cli handler with some
      better explanations.

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,75 +1,28 @@
 const assert = require('assert');
-const { modulePath, writeJSON, readFile, formatCompileError } = require('../lib/utils');
+const {
+  modulePath,
+  writeJSON,
+  readFile,
+  formatCompileError,
+} = require('../lib/utils');
 
-describe("Utils", () => {
-
-  it(".modulePath", () => {
-
-    let matches;
-
-    matches = modulePath("language-salesforce")
-    assert.deepEqual(matches, {
-      path: 'language-salesforce',
-      memberName: null,
-      isRelative: false
-    })
-
-    matches = modulePath("language-salesforce.Adaptor")
-    assert.deepEqual(matches, {
-      path: 'language-salesforce',
-      memberName: 'Adaptor',
-      isRelative: false
-    })
-
-    matches = modulePath("./language-salesforce.Adaptor")
-    assert.deepEqual(matches, {
-      path: './language-salesforce',
-      memberName: 'Adaptor',
-      isRelative: true
-    })
-
-    matches = modulePath("./path/to/language-salesforce.Adaptor")
-    assert.deepEqual(matches, {
-      path: './path/to/language-salesforce',
-      memberName: 'Adaptor',
-      isRelative: true
-    })
-
-    matches = modulePath("./path/to/language-salesforce-v1.0.0.Adaptor")
-    assert.deepEqual(matches, {
-      path: './path/to/language-salesforce-v1.0.0',
-      memberName: 'Adaptor',
-      isRelative: true
-    })
-
-    matches = modulePath("/path/to/language-salesforce-v1.0.0.Adaptor")
-    assert.deepEqual(matches, {
-      path: '/path/to/language-salesforce-v1.0.0',
-      memberName: 'Adaptor',
-      isRelative: false
-    })
-
-  })
-
-  it(".writeJSON", () => {
-    return writeJSON('/tmp/output.json', {a: 1})
+describe('Utils', () => {
+  it('.writeJSON', () => {
+    return writeJSON('/tmp/output.json', { a: 1 })
       .then(() => readFile('/tmp/output.json'))
-      .then((str) => assert.deepEqual({a: 1}, JSON.parse(str)))
-  })
+      .then(str => assert.deepEqual({ a: 1 }, JSON.parse(str)));
+  });
 
-  it(".formatCompileError", () => {
-    let expected = [
-    "Line 1: foo()",
-    "        ^^^^^",
-    "Bad!"
-    ].join("\n")
+  it('.formatCompileError', () => {
+    let expected = ['Line 1: foo()', '        ^^^^^', 'Bad!'].join('\n');
 
-    let result = formatCompileError("foo()", {
-      node: { loc: { start: { line: 1, column: 0 }, end: { line: 1, column: 5 } } },
-      message: "Bad!"
-    })
+    let result = formatCompileError('foo()', {
+      node: {
+        loc: { start: { line: 1, column: 0 }, end: { line: 1, column: 5 } },
+      },
+      message: 'Bad!',
+    });
 
     assert.equal(expected, result);
-  })
-})
-
+  });
+});
