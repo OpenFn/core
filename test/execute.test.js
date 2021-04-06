6bc15688a6b875b2ce72569095f1ee2a0a89ebfc
Stuart Corbishley <corbish@gmail.com>
2021-04-06 14:32:30 +0200

    Formatting

--- test/execute.test.js
+++ test/execute.test.js
@@ -1,30 +1,31 @@
 const assert = require('assert');
 const Execute = require('../lib/execute');
 
-describe("Execute", () => {
-  it("expects an expression", () => {
-    assert.throws(
-      () => { Execute({}) },
-      /Cannot execute without an expression./
-    )
-  })
+describe('Execute', () => {
+  it('expects an expression', () => {
+    assert.throws(() => {
+      Execute({});
+    }, /Cannot execute without an expression./);
+  });
 
-  it("expects an initial state", () => {
-    assert.throws(
-      () => { Execute({ expression: "foo" }) },
-      /Cannot execute without an initial state./
-    )
-  })
+  it('expects an initial state', () => {
+    assert.throws(() => {
+      Execute({ expression: 'foo' });
+    }, /Cannot execute without an initial state./);
+  });
 
-  describe("when given an expression and state", () => {
-    it("returns", () => {
+  describe('when given an expression and state', () => {
+    it('returns', () => {
       let result = Execute({
         expression: `add(1)(state)`,
         state: 1,
-        sandbox: { add: num => state => { return state + num } }
-      })
-      assert.equal(result, 2)
-
-    })
-  })
-})
+        sandbox: {
+          add: num => state => {
+            return state + num;
+          },
+        },
+      });
+      assert.equal(result, 2);
+    });
+  });
+});
